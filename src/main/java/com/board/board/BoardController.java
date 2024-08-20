f8c69c617323d74d9a9f2ae8025422ed22da6176
intae0099 <dlsxo14700@gmail.com>
2024-08-20 19:41:40 +0900

    List Pagination 구현 nav바 구현

--- src/main/java/com/board/board/BoardController.java
+++ src/main/java/com/board/board/BoardController.java
@@ -1,6 +1,10 @@
 package com.board.board;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -27,6 +31,20 @@ public class BoardController {
     @GetMapping("/list")
     public String list(Model model){
         model.addAttribute("posts", postRepository.findAll());
+        return "list.html";
+    }
+
+    @GetMapping("/list/page/{pagenum}")
+    public String pagelist(Model model, @PathVariable Integer pagenum){
+        Page<Post> result = postRepository.findPageBy(PageRequest.of(pagenum - 1, 5, Sort.by("id").descending()));
+        int nowPage = result.getPageable().getPageNumber() + 1;
+        int startPage = Math.max(nowPage - 4, 1);
+        int endPage = Math.min(nowPage+9, result.getTotalPages());
+
+        model.addAttribute("posts", result);
+        model.addAttribute("nowPage", nowPage);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
         return "list.html";
     }
 
--- src/main/java/com/board/board/PostRepository.java
+++ src/main/java/com/board/board/PostRepository.java
@@ -1,7 +1,9 @@
 package com.board.board;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-
+    Page<Post> findPageBy(Pageable page);
 }
